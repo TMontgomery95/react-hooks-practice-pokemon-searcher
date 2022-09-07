38388c637ba6985185c04c1da8b399f6d1f814a7
Timothy Montgomery <tim.montgomery95@gmail.com>
2022-09-07 12:04:21 -0400

    pokemon-practice-cc-wip

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [pokemon, setPokemon] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/pokemon")
+    .then(response => response.json())
+    .then(data => setPokemon(data))
+  }, [])
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
